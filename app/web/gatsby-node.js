bd49e9daec4b3a11355d011f94c36589c6b852cd
kodless <admin@kodhive.com>
2022-04-11 14:36:39 +0000

    hotfix - force app data cache invalidation

--- app/web/gatsby-node.js
+++ app/web/gatsby-node.js
@@ -1,3 +1,8 @@
+const path = require(`path`)
+const glob = require('glob')
+const md5 = require('md5')
+const fs = require('fs-extra')
+
 /**
  * Implement Gatsby's Node APIs in this file.
  *
@@ -19,3 +24,23 @@ exports.onCreateWebpackConfig = (helper) => {
     actions.replaceWebpackConfig(config);
   }
 };
+
+exports.onPostBuild = async () => {
+  const publicPath = path.join(__dirname, "public")
+  const hash = md5(Math.random().toString(36).substring(7))
+
+  const htmlAndJSFiles = glob.sync(`${publicPath}/**/*.{html,js}`)
+  console.log(
+      "[onPostBuild] Replacing page-data.json references in the following files:"
+  )
+  for (let file of htmlAndJSFiles) {
+    const stats = await fs.stat(file, "utf8")
+    if (!stats.isFile()) continue
+    console.log(file)
+    let content = await fs.readFile(file, "utf8");
+    let result = content
+        //.replace(/page-data.json/g, `page-data.json/?${hash}`)
+        .replace(/app-data.json/g, `app-data.json/?${hash}`);
+    await fs.writeFile(file, result, "utf8")
+  }
+}
