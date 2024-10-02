d23ee28bd8093a419e561071990f37a41242b0b6
Hamza Adami <56397909+kodless@users.noreply.github.com>
2024-10-02 14:55:12 +0100

    Auto create app if not exist (#88)
    * Auto-Create app if not exist
    
    * Auto-Create app if not exist

--- app/leek/api/routes/events.py
+++ app/leek/api/routes/events.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from flask import Blueprint, request, g
 from flask_restx import Resource
@@ -10,6 +11,7 @@ from leek.api.db.events import merge_events
 from leek.api.errors import responses
 from leek.api.routes.api_v1 import api_v1
 from leek.api.db.template import get_app
+from leek.api.db import template as apps
 
 events_bp = Blueprint('events', __name__, url_prefix='/v1/events')
 events_ns = api_v1.namespace('events', 'Agents events handler')
@@ -43,14 +45,38 @@ class ProcessEvents(Resource):
         except KeyError as e:
             return responses.missing_headers
 
+        template_name = f"{org_name}-{app_name}"
+
         try:
             # Get application
-            app = get_app(f"{org_name}-{app_name}")
+            app = get_app(template_name)
             # Authenticate
             if app_key not in [app["app_key"], settings.LEEK_AGENT_API_SECRET]:
                 return responses.wrong_application_app_key
         except es_exceptions.NotFoundError:
-            return responses.application_not_found
+            # Create app if not exist, only supported when leek auth is disabled.
+            if settings.LEEK_CREATE_APP_IF_NOT_EXIST and not settings.LEEK_API_ENABLE_AUTH:
+                logger.warning("LEEK_CREATE_APP_IF_NOT_EXIST was set to true, leek app will be auto-created by agent.")
+                app = {
+                    "app_name": app_name,
+                    "app_description": "auto created app by agent",
+                    "fo_triggers": [],
+                    "owner": "public",
+                    "app_key": settings.LEEK_AGENT_API_SECRET,
+                    "created_at": int(round(time.time() * 1000))
+                }
+                _, status_code = apps.create_index_template(
+                    index_alias=template_name,
+                    number_of_shards=1,
+                    number_of_replicas=0,
+                    lifecycle_policy_name="leek-rollover-policy",
+                    meta=app
+                )
+                if status_code == 201:
+                    logger.info("leek app auto-created successfully.")
+                    return "Ready!", 200
+            else:
+                return responses.application_not_found
         except es_exceptions.ConnectionError:
             return responses.search_backend_unavailable
 
